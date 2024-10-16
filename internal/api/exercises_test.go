32350b93e1ac413b50398e8511c1691a4f7423bc
mosamadeeb <mosamaeldeeb@gmail.com>
2024-10-16 12:08:38 +0300

    modularize testing logic

--- internal/api/exercises_test.go
+++ internal/api/exercises_test.go
@@ -1,11 +1,8 @@
 package api
 
 import (
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
-	"strings"
 	"testing"
 )
 
@@ -13,71 +10,28 @@ func Test_ApiExercises(t *testing.T) {
 	state := prepTestState()
 	handler := prepTestHandler(state, handleApiExercises)
 
-	type args struct {
-		w *httptest.ResponseRecorder
-		r http.Request
-	}
 	tests := []struct {
-		name         string
-		args         args
-		expectedCode int
-		expectedType string
-		expectedBody string
+		name     string
+		args     apiTestArgs
+		expected apiTestExpected
 	}{
 		{
-			"Fetch exercise by ID",
-			args{nil, *httptest.NewRequest(http.MethodGet, "/api/exercises/1", nil)},
-			http.StatusOK,
-			"application/json",
-			`{
-				"id": 1,
-				"name": "Pushups",
-				"description": "A pushup is a bodyweight exercise where you lower and raise your body using your arms while keeping your back straight."
-			}`,
+			"fetch exercise by ID",
+			apiTestArgs{nil, *httptest.NewRequest(http.MethodGet, "/api/exercises/1", nil)},
+			apiTestExpected{
+				http.StatusOK,
+				"application/json",
+				[]byte(`{
+					"id": 1,
+					"name": "Pushups",
+					"description": "A pushup is a bodyweight exercise where you lower and raise your body using your arms while keeping your back straight."
+				}`),
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if tt.args.w == nil {
-				tt.args.w = httptest.NewRecorder()
-			}
-
-			handler.ServeHTTP(tt.args.w, &tt.args.r)
-
-			res := tt.args.w.Result()
-			defer res.Body.Close()
-
-			if res.StatusCode != tt.expectedCode {
-				t.Logf("Expected status code %d but got %d\n", tt.expectedCode, res.StatusCode)
-
-				if message, ok := checkApiError(res); ok {
-					t.Logf("Error message: %s\n", message)
-				}
-
-				t.FailNow()
-			}
-
-			contentType := res.Header.Get("Content-Type")
-			if tt.expectedType != "" && contentType != tt.expectedType {
-				t.Logf("Expected content type %s but got %s\n", tt.expectedType, contentType)
-				t.FailNow()
-			}
-
-			switch contentType {
-			case "application/json":
-				var wantStruct, gotStruct map[string]interface{}
-
-				expectedJson := json.NewDecoder(strings.NewReader(tt.expectedBody))
-				resultJson := json.NewDecoder(res.Body)
-
-				expectedJson.Decode(&wantStruct)
-				resultJson.Decode(&gotStruct)
-
-				if !reflect.DeepEqual(wantStruct, gotStruct) {
-					t.Logf("Expected json body %v but got %v\n", wantStruct, gotStruct)
-					t.FailNow()
-				}
-			}
+			apiTestHelper(t, handler, tt.args, tt.expected)
 		})
 	}
 }
--- internal/api/test_common.go
+++ internal/api/test_common.go
@@ -1,11 +1,16 @@
 package api
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
+	"net/http/httptest"
 	"os"
+	"reflect"
+	"testing"
 
 	"github.com/joho/godotenv"
 	"github.com/mosamadeeb/go-workout-tracker/internal/database"
@@ -35,4 +40,60 @@ func checkApiError(r *http.Response) (string, bool) {
 	}
 
 	return "", false
+}
+
+type apiTestArgs struct {
+	w *httptest.ResponseRecorder
+	r http.Request
+}
+
+type apiTestExpected struct {
+	code        int
+	contentType string
+	body        []byte
+}
+
+func apiTestHelper(t *testing.T, handler http.Handler, args apiTestArgs, expected apiTestExpected) {
+	t.Helper()
+
+	if args.w == nil {
+		args.w = httptest.NewRecorder()
+	}
+
+	handler.ServeHTTP(args.w, &args.r)
+
+	res := args.w.Result()
+	defer res.Body.Close()
+
+	if res.StatusCode != expected.code {
+		t.Logf("Expected status code %d but got %d\n", expected.code, res.StatusCode)
+
+		if message, ok := checkApiError(res); ok {
+			t.Logf("Error message: %s\n", message)
+		}
+
+		t.FailNow()
+	}
+
+	contentType := res.Header.Get("Content-Type")
+	if expected.contentType != "" && contentType != expected.contentType {
+		t.Logf("Expected content type %s but got %s\n", expected.contentType, contentType)
+		t.FailNow()
+	}
+
+	switch contentType {
+	case "application/json":
+		var wantStruct, gotStruct map[string]interface{}
+
+		expectedJson := json.NewDecoder(bytes.NewReader(expected.body))
+		resultJson := json.NewDecoder(res.Body)
+
+		expectedJson.Decode(&wantStruct)
+		resultJson.Decode(&gotStruct)
+
+		if !reflect.DeepEqual(wantStruct, gotStruct) {
+			t.Logf("Expected json body %v but got %v\n", wantStruct, gotStruct)
+			t.FailNow()
+		}
+	}
 }
